d077c371e1406259e16709d50d4f1ed58d7f91e1
magohipnotista <62730351+magohipnotista@users.noreply.github.com>
2022-09-11 12:28:30 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <React.Strictmode>
     <App />
-  </>
+  <React.Strictmode/>
 )
